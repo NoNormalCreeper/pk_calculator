a6562ca7a5add8c3b3347fc698958219436af14c
NoNormalCreeper <2560359315@qq.com>
2024-11-24 19:14:54 +0800

    avoid div by 0

--- utils/calculator.py
+++ utils/calculator.py
@@ -4,4 +4,6 @@ def generate_challenge() -> str:
     number1 = random.randint(1, 10)
     number2 = random.randint(1, 10)
     operator = random.choice(['+', '-', '*', '/'])
+    if operator == '/' and number2 == 0:
+        return generate_challenge()
     return f'{number1} {operator} {number2}'
